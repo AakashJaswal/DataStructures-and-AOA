2ac8e0d228dad84ef4395d39a3d60145ba1551d0
Aakash <jaswal.aakash.14ce1022@gmail.com>
2020-11-18 02:41:01 +0530

    added main call

---
+++ Java/src/LinkedList/HandsOn/Main.java
@@ -0,0 +1,12 @@
+package LinkedList.HandsOn;
+
+public class Main {
+    public static void main (String[] args){
+        LinkedList<Integer> LL = new LinkedList<>();
+        LL.display();
+        LL.addNode(2);
+        LL.addNode(3);
+        LL.addNode(4);
+        LL.display();
+    }
+}
