1349f05f7d6e64a84e05ec356a52b5f17fb2c762
Aakash <jaswal.aakash.14ce1022@gmail.com>
2023-06-08 00:11:37 -0400

    recursion two branch with memo

--- Python/AOA/recursion.py
+++ Python/AOA/recursion.py
@@ -10,3 +10,23 @@ def fact2(n: int) -> int:
 
 
 print(fact2(3))
+
+
+def fibo(data: int) -> int:
+    if data in [0, 1]:
+        return data
+    return fibo(data - 1) + fibo(data - 2)
+
+
+def fibo_memo(data: int, memo: dict = {}) -> int:
+    if data in [0, 1]:
+        return data
+    if data in memo:
+        return memo[data]
+
+    memo[data] = fibo(data - 1) + fibo(data - 2)
+    return memo[data]
+
+
+print(fibo(6))
+print(fibo_memo(6))
