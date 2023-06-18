76c8d3bb2e3d13d68f90b9935d825bac6bd695f2
Aakash <jaswal.aakash.14ce1022@gmail.com>
2023-06-18 02:51:30 -0400

    quick sort

---
+++ Python/AOA/sort/quick_sort.py
@@ -0,0 +1,23 @@
+n = [4, 3, 2, 1]
+
+
+def quick_sort(nums: list[int], s, e):
+    if (e - s + 1 <= 1):
+        return nums
+
+    left, start, pivot = s, s, e
+
+    for i in range(s, e):
+        if nums[i] < nums[pivot]:
+            nums[i], nums[left] = nums[left], nums[i]
+            left += 1
+
+    nums[pivot], nums[left] = nums[left], nums[pivot]
+
+    quick_sort(nums, s, left - 1)
+    quick_sort(nums, left + 1, pivot)
+
+    return nums
+
+
+print(quick_sort(n, 0, len(n)-1))
