9a50b9dc032ca304db7170c98123ac814e1c2d36
Aakash <jaswal.aakash.14ce1022@gmail.com>
2023-07-12 15:34:50 -0400

    dutch flag

---
+++ Python/AOA/multiple pointers/dutch_flag.py
@@ -0,0 +1,16 @@
+nums = [2, 0, 2, 1, 1, 0]
+l, m, h = 0, 0, len(nums) - 1
+
+while m < h:
+    if nums[m] == 0:
+        nums[l], nums[m] = nums[m], nums[l]
+        l += 1
+        m += 1
+    elif nums[m] == 1:
+        m += 1
+    else:
+        nums[h], nums[m] = nums[m], nums[h]
+        h -= 1
+
+
+print(nums)
